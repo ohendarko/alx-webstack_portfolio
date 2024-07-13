adc29f75ee706062c5f08b510ca75cb4e2ec793e
ohendarko <ohendarko@gmail.com>
2024-07-13 21:31:26 +0000

    used imports

--- meme-generator/src/Components/Meet.jsx
+++ meme-generator/src/Components/Meet.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Prince from '../assets/Prince.png';
+import Kwadwo from '../assets/Kwadwo.png';
+
 
 const Meet = () => {
   return (
@@ -13,7 +16,7 @@ const Meet = () => {
     <h2>About Us</h2>
     <div class="team">
       <div class="member">
-        <img src="/src/assets/Prince.png" alt="Dr. Appiagyei Osei" class="pic" />
+        <img src={Prince} alt="Dr. Appiagyei Osei" class="pic" />
         <div class="text">
           <h3>Dr. Appiagyei Osei</h3>
           <h5>Front-end Engineer</h5>
@@ -40,7 +43,7 @@ const Meet = () => {
         </div>
       </div>
       <div class="member">
-        <img src="assets/Kwame.jpg" alt="Kwame Adobaw" class="pic" />
+        <img src="src/assets/Kwame" alt="Kwame Adobaw" class="pic" />
         <div class="text">
           <h3>Kwame Adobaw</h3>
           <h5>Back-end Engineer</h5>
@@ -67,7 +70,7 @@ const Meet = () => {
         </div>
       </div>
       <div class="member">
-        <img src="/src/assets/Kwadwo.png" alt="Kwadwo Darko" class="pic" />
+        <img src={Kwadwo} alt="Kwadwo Darko" class="pic" />
         <div class="text">
           <h3>Dr. Kwadwo Ohene Darko</h3>
           <h5>Front-end Engineer/REACT Specialist</h5>
@@ -115,7 +118,7 @@ const Meet = () => {
               <a href="https://github.com/AyandaNgubane" target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg></a>
             </li>
           </div>
-          <p>Ayanda Ngubane oversees the development and deployment of Meme Generator. With a focus on project planning and coordination, she ensures that our team meets deadlines and delivers a high-quality product that exceeds user expectations.</p>
+          <p>Ayanda Ngubane oversees the development and deployment of Meme Generator. With a focus on project planning and coordination, he ensures that our team meets deadlines and delivers a high-quality product that exceeds user expectations.</p>
         </div>
       </div>
       <div class="contact member">
